9754978acf9d3dcd3cbb7da3515c9884390d5c19
gabriel <gabf48@gmail.com>
2026-02-24 22:11:32 +0200

    added versions

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,20 @@
 import "./globals.css";
 import Header from "@/components/Header";
+import Footer from '@/components/Footer';
 import { AuthProvider } from "@/app/context/AuthContext";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ro">
-      <body className="space-bg min-h-screen">
+      <body className="space-bg min-h-screen flex flex-col">
         <AuthProvider>
           <Header />
-          {children}
+          
+          {/* main content */}
+          <main className="flex-1">{children}</main>
+
+          {/* footer fix */}
+          <Footer />
         </AuthProvider>
       </body>
     </html>
