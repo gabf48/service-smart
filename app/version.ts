91c700d796b9e5f3f3f594c42b8c00225c560959
Gabriel Filip <37806829+gabf48@users.noreply.github.com>
2026-02-26 23:40:28 +0200

    chore: bump version to 1.0.2 (#9) (#10)

--- app/version.ts
+++ app/version.ts
@@ -3,16 +3,21 @@ export const versions = [
   {
     version: "1.0.0",
     date: "2026-02-24",
-    changes: ["Versiune de bază"]
+    changes: ["Versiune de bază"],
   },
   {
     version: "1.0.1",
     date: "2026-02-24",
-    changes: ["Pagina contact reparată", "Footer adăugat"]
+    changes: [
+      "Pagina Contact reparată",
+      "Footer adăugat",
+      "Modal versiuni adăugat",
+      "Scroll și design mai curat",
+    ],
   },
   {
-    version: "1.0.1",
-    date: "2026-02-24",
-    changes: ["Adăugat modal versiuni", "Scroll și design mai curat"]
-  }
+    version: "1.0.2",
+    date: "2026-02-26",
+    changes: ["Titlu în tab (metadata) + favicon configurat"],
+  },
 ];
