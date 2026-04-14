2ae51173c5ad060fa9d56bf08644c02f32e70a61
Gabriel Filip <37806829+gabf48@users.noreply.github.com>
2026-04-14 12:29:00 +0300

    Stg (#31)
    * 2FA
    
    * fixed automatic tests
    
    * fix automatic test
    
    * fixed automatic tests
    
    * useLoginSubmit.ts temporar
    
    * temporar fix
    
    * update e2e
    
    * test
    
    * test
    
    * disable auth + disable e2e
    
    * fix yaml file
    
    * client version
    
    * google analystic

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,13 +3,14 @@ import AppVersion from "@/components/AppVersion";
 import { AuthProvider } from "@/app/context/AuthContext";
 import type { Metadata } from "next";
 import LayoutShell from "@/components/LayoutShell";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: {
     default: "Service Smart",
     template: "%s | Service Smart",
   },
-  description: "Service Smart",
+  description: "Reparații laptop și PC în Cluj-Napoca. Rapid, corect și fără complicații.",
   icons: {
     icon: "/favicon.ico",
   },
@@ -22,9 +23,24 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ro">
+      <head>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-XXXXXXX"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-65W4QTQDY7');
+          `}
+        </Script>
+      </head>
+
       <body className="space-bg min-h-screen flex flex-col">
         <AuthProvider>
-         <LayoutShell>{children}</LayoutShell>
+          <LayoutShell>{children}</LayoutShell>
         </AuthProvider>
       </body>
     </html>
